d749a7e8c36c8feaca9b0bb9a7cdcda0124b1877
seojbin <seojbin@gmail.com>
2021-10-18 16:39:17 +0900

    seo

--- sample.py
+++ sample.py
@@ -2,4 +2,9 @@ for i in range(5):
     for j in range(i+1):
         print('*', end = "")
     print('')
+<<<<<<< HEAD
 
+=======
+ \
+print("This is modified file")
+>>>>>>> parent of d866227... second commit
